fab65689efae814911a87f0daba446434d9448fe
Hitesh Salunkhe <85603738+salunkhe12@users.noreply.github.com>
2023-11-10 18:50:06 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,20 @@
+class Boolean
+{
+	public static void main(String args [])
+	{
+		int a  = 5;
+		int b =  10;
+		boolean c = a++ < 10 || b++<11;
+		//boolan d  = a++ < 10 && b++< 5;
+		System.out.println(c);
+		System.out.println(a);
+  		System.out.println(b);
+	}
+
+
+} 
+/*
+
+
+
+*/
