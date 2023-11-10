52d784a90ba447016e50b8fd8af3b5ef5c0b703a
Hitesh Salunkhe <85603738+salunkhe12@users.noreply.github.com>
2023-11-10 18:49:34 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,15 @@
+class Average
+{
+	public static void main(String args [])
+	{
+		int s1 = 50;
+		int s2 = 80;
+		int s3 = 70;
+		double add = s1 + s2 + s3;
+                double avg = add/3;
+                System.out.println("it is Average of 3 Students Marks = " + avg);
+
+
+
+	}
+}
