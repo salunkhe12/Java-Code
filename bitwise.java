7f14c877d7e4d80aff84b13969bb3dc0157c47a0
Hitesh Salunkhe <85603738+salunkhe12@users.noreply.github.com>
2023-11-22 22:13:27 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,14 @@
+class bitwise
+{
+public static void main(String args [])
+{
+int x =13,y =15; 
+
+ System.out.println(x&y);
+ System.out.println(x|y);
+ System.out.println(x^y);
+ int c = 7, d = 3;
+ System.out.println(c>>2);
+ System.out.println(c<<d);
+}
+}
