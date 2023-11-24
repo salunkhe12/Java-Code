3a5b0c97edd25492c69134191515abaac2c0d494
Hitesh Salunkhe <85603738+salunkhe12@users.noreply.github.com>
2023-11-24 23:30:00 +0530

    Add files via upload

---
+++ sumofalldivisor.java
@@ -0,0 +1,18 @@
+class sumofalldivisor
+{
+ public static void main(String args [])
+ {
+  int a = 4;
+  
+  int i = 1;
+  
+  int sum = 0;
+  
+  while(n != 0)
+  {
+   sum = sum + (n/i)*i++;
+  }
+  
+  System.out.println(sum + " is Sum of all divisors");
+ }
+}
